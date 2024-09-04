bcf76f1b3d5331e1362ffbe7a15def19f6eb0537
Victor-MFr <victorfragoso88@gmail.com>
2024-09-04 00:24:52 -0300

    Atualização e conclusão do projeto da DIO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
+        private string Imei;
+        private int Memoria;
 
-        public Smartphone(string numero)
+        
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -22,5 +27,10 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
+        public void InformacoesdoSmartphone()
+        {
+            Console.WriteLine($"Numero de telefone: {Numero}\nModelo: {Modelo}\nIMEI: {Imei}\nMemória: {Memoria.ToString($"{Memoria}Gb")}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone cellNokia = new Nokia("0000000000", "Windows 3", "15532F36", 128);
+Smartphone cellIphone = new Iphone("0000000000", "X", "Q5R32F36", 64);
+
+cellNokia.InformacoesdoSmartphone();
+cellIphone.InformacoesdoSmartphone();
+
+cellNokia.InstalarAplicativo("Twitter");
+cellIphone.InstalarAplicativo("Steam");
+
+cellNokia.Numero = "0123456789";
+cellIphone.Numero = "9876543210";
+
+cellNokia.Ligar();
+cellIphone.Ligar();
+
+cellNokia.ReceberLigacao();
+cellIphone.ReceberLigacao();
+
+
+cellNokia.InformacoesdoSmartphone();
+cellIphone.InformacoesdoSmartphone();
